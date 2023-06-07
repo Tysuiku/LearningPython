dc1ad629fbedbca485a134d70c7c94aa82191b4b
Tysuiku <timothydong41@gmail.com>
2023-06-07 18:34:19 -0400

    basics of python

--- testing.py
+++ testing.py
@@ -1,1 +1,24 @@
 # print("hello world")
+# def test_function():
+#     print("hello from my function")
+
+# test_function()
+
+# apple = "apple is red"
+# apple2 = "   ""apple is red""   "
+
+# print(apple)
+# print(apple2)
+
+# \ here extends long statements 
+
+# if(1 < 2) and (2 > 1) and \
+#     (1 == 1):
+#     print("multi-line statement is working")
+
+# casing
+# pythonSyntax = camel case
+# PythonSyntax = pascal case is used for class names
+# python_syntax = snake case is used for variables, functions, methods, and modules
+# PYTHON_SYNTAX = used for constants
+
