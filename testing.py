74dfbccc0e1f7020726bf5fd07a5989753bba1c5
Tysuiku <timothydong41@gmail.com>
2023-06-08 16:57:55 -0400

    learning python syntax

--- testing.py
+++ testing.py
@@ -22,3 +22,66 @@
 # python_syntax = snake case is used for variables, functions, methods, and modules
 # PYTHON_SYNTAX = used for constants
 
+# name = input("what is you name?")
+# print (name)
+
+# fruit = ["apple", "grapes", "pear","watermelon"]
+# findfruit = "banana" in fruit
+# print (findfruit)
+
+# 10 / 3 returns float
+# 10 // 3 returns whole
+# 10 ** 3 10 to the power of 3 so 1000 exponent operator 
+
+# price = 10
+# print (not price < 15)
+# print (price == 5 or price < 11)
+# print(price == 10 and price > 5)
+
+# food = ["sandwidch", "hotdog", "cucumber", "hotpot", "bbq"]
+
+# if "pizza" in food:
+#     print("we have got pizza")
+# elif "sushi" in food:
+#     print("no pizza but we have sushi")
+# else:
+#     print("no food we like")
+
+# i = 0
+# while i <= 5:
+#     print(i)
+#     i = i + 1
+
+vtubers = ["suisei","aqua","towa","pekora"]
+# vtubers.append("chloe") adds input to end of list
+# vtubers.insert(0, "miko") insert to index position on list
+# vtubers.remove("towa") removes input value
+# vtubers.clear() clears the entire list
+# print(len(vtubers))
+# for vtuber in vtubers:
+#     print(vtuber)
+
+# i = 0
+# while i < len(vtubers):
+#     print(vtubers[i])
+#     i = i + 1
+
+# nums = range(5,10,3)
+# for num in nums:
+#     print(num)
+
+# numbers = [1,2,3,4,5,6,7,8,9]
+
+# for i in range(len(numbers)):
+#     print(numbers[i])
+
+# numbers = (1,2,3,2) tuple
+# print(numbers.count(2))
+# print(numbers.index(1))
+
+hashmap = {"suisei" : "star", 
+           "aqua" : "ship",
+           "towa": "devil"
+           }
+
+print(hashmap["suisei"])
